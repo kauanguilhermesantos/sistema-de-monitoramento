9341f86f22948a4186e37872d6022800222e666d
kauanguilhermesantos <kauansantos1411@outlook.com>
2024-11-23 10:50:57 -0300

    feat: adicionando função pegaInfoMemoria

--- app.py
+++ app.py
@@ -7,13 +7,22 @@ def pegaInfoCpu():
     nucleosFisicos = psutil.cpu_count(logical=False)
     print("CPU:")
     print(f"Núcleos lógicos: {nucleosLogicos}")
-    print(f"Núcleos físicos: {nucleosFisicos}")
+    print(f"Núcleos físicos: {nucleosFisicos}\n")
 
 def pegaInfoMemoria():
-    memoria = psutil.virtual_memory()
+    memoriaTotal = psutil.virtual_memory().total
+    memoriaDisponivel = psutil.virtual_memory().available
+    memoriaUsada = psutil.virtual_memory().used
+    memoriaLivre = psutil.virtual_memory().free
+    memoriaUsadaPercentual = psutil.virtual_memory().percent
     
-    print("Memória")
-    print(memoria)
+    print("Memória RAM:")
+    print(f"Total: {memoriaTotal / (1024 ** 3):.2f} GB")
+    # print(f"Disponível: {memoriaDisponivel / (1024 ** 3):.2f} GB")
+    print(f"Usada: {memoriaUsada / (1024 ** 3):.2f} GB")
+    print(f"Livre: {memoriaLivre / (1024 ** 3):.2f} GB")
+    print(f"Percetual usado: {memoriaUsadaPercentual:.2f} %")
+    
 
 pegaInfoCpu()
 pegaInfoMemoria()
